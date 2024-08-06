9cff9b6e632bea26ff2dcbebfff96b5c6206199b
Ryan <nguyenduylan711@gmail.com>
2024-08-06 17:57:12 +0700

    add category page

--- src/Pages/Staff/Category/StaffCategoryListingPage/index.tsx
+++ src/Pages/Staff/Category/StaffCategoryListingPage/index.tsx
@@ -3,12 +3,20 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Navigate, useNavigate } from "react-router-dom";
 
+import { Flex, Button } from "antd";
+import { CategoryTable } from "../../../../Components/Tables";
+
 
 const StaffCategoryListingPage: React.FC = () => {
     return (
-        <>
-            <h1>Staff - Category Listing Page</h1>
-        </>
+        <div className="staff-category-listing-page">
+            <Flex gap="small" vertical>
+                <Flex justify="flex-end" gap="small">
+                    <Button>Add Category</Button>
+                </Flex>
+                <CategoryTable />
+            </Flex>
+        </div>
     )
 };
 
