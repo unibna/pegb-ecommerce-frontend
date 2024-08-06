51f2ef744cf8c8bfb42afc0f2704292f8c480a04
Ryan <nguyenduylan711@gmail.com>
2024-08-06 17:57:58 +0700

    update router for category, product, promotion

---
+++ src/Components/Tables/index.ts
@@ -0,0 +1,5 @@
+import CategoryTable from "./CategoryTable";
+import ProductTable from "./ProductTable";
+import PromotionTable from "./PromotionTable";
+
+export { CategoryTable, ProductTable, PromotionTable };
