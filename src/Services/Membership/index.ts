ea2ab0150f12605865f5f5b204a9923519ba5ea3
Ryan <nguyenduylan711@gmail.com>
2024-08-07 13:28:20 +0700

    add membership service

---
+++ src/Services/Membership/index.ts
@@ -0,0 +1,102 @@
+import axios from "axios";
+
+import AuthService from "../Auth";
+
+
+const MEMBERSHIP_API_URL = process.env.REACT_APP_API_URL + '/memberships';
+
+
+const list = async (): Promise<any> => {
+    try {
+        const headers = AuthService.getHeader();
+        const response = await axios.get(
+            MEMBERSHIP_API_URL + '/',
+            { headers: headers }
+        )
+        const data = await response.data;
+        return data;
+    } catch (error: any) {
+        if (error.response) {
+            const errors = error.response.data;
+            const errorMessage = `${Object.keys(errors)[0]}: ${errors[Object.keys(errors)[0]]}`
+            throw new Error(errorMessage);
+        } else {
+            throw error;
+        }
+    }
+};
+
+
+const get = async (promotionId: string): Promise<any> => {
+    try {
+        const headers = AuthService.getHeader();
+        const response = await axios.get(
+            MEMBERSHIP_API_URL + `/${promotionId}/`,
+            { headers: headers }
+        )
+        const data = await response.data;
+        return data;
+    } catch (error: any) {
+        if (error.response) {
+            const errors = error.response.data;
+            const errorMessage = `${Object.keys(errors)[0]}: ${errors[Object.keys(errors)[0]]}`
+            throw new Error(errorMessage);
+        } else {
+            throw error;
+        }
+    }
+}
+
+
+const create = async (payload: any): Promise<any> => {
+    try {
+        const headers = AuthService.getHeader();
+        const response = await axios.post(
+            MEMBERSHIP_API_URL + '/',
+            payload,
+            { headers: headers }
+        )
+        const data = await response.data;
+        return data;
+    } catch (error: any) {
+        if (error.response) {
+            const errors = error.response.data;
+            const errorMessage = `${Object.keys(errors)[0]}: ${errors[Object.keys(errors)[0]]}`
+            throw new Error(errorMessage);
+        } else {
+            throw error;
+        }
+    }
+};
+
+
+const update = async (id: string, payload: any): Promise<any> => {
+    try {
+        const headers = AuthService.getHeader();
+        const response = await axios.put(
+            MEMBERSHIP_API_URL + `/${id}/`,
+            payload,
+            { headers: headers }
+        )
+        const data = await response.data;
+        return data;
+    } catch (error: any) {
+        if (error.response) {
+            const errors = error.response.data;
+            const errorMessage = `${Object.keys(errors)[0]}: ${errors[Object.keys(errors)[0]]}`
+            throw new Error(errorMessage);
+        } else {
+            throw error;
+        }
+    }
+}
+
+
+const ProductService = {
+    list,
+    get,
+    create,
+    update,
+};
+
+export default ProductService;
--- src/Services/index.ts
+++ src/Services/index.ts
@@ -1,11 +1,13 @@
 import AuthService from "./Auth";
 import CategoryService from "./Category";
+import MembershipService from "./Membership";
 import ProductService from "./Product";
 import PromotionService from "./Promotion";
 
 export { 
     AuthService,
     CategoryService,
+    MembershipService,
     ProductService,
     PromotionService,
 };
